5762ab26d8f16e87201371ec68797ac00a786e89
joast888 <61902692+joast888@users.noreply.github.com>
2020-12-26 21:52:09 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,3 +15,4 @@
 
 # Add a feed source
 sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+git clone https://github.com/jefferymvp/luci-app-koolproxyR package/luci-app-koolproxyR
